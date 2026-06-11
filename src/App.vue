56bcc2138a2b611c5483672eddbee79a224c4a04
Simon <simon102756@gmail.com>
2026-06-11 09:38:20 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app">
+    <h2>app</h2>
+    <router-link to="/main">主要</router-link>
+    <router-link to="/login">登录</router-link>
+    <router-view />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
